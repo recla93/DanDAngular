fe181f480a7f93d545c0907ada122258de5e945b
recla93 <recla93@gmail.com>
2025-05-12 16:03:51 +0200

    Background + repo monster + glossary ongoing + implementazione stili generici

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'DanDAngular';
+  darkMode = false;
+
+  toggleDark() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark-mode', this.darkMode);
+  }
 }
+
--- src/components/gameArch/glossary/glossary.component.ts
+++ src/components/gameArch/glossary/glossary.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Pg} from '../../../model/Pg';
 import {PgRepositoryService} from '../../../services/pg-repository.service';
-import * as Console from 'node:console';
 import {Monster} from '../../../model/Monster';
 import {MonsterRepositoryService} from '../../../services/monster-repository.service';
 
@@ -20,9 +19,7 @@ export class GlossaryComponent
               private monsterRepo:MonsterRepositoryService)
   {
     pgRepo.getAllPgs().subscribe(resp=> this.personaggi = resp);
-    Console.log("Personaggi caricati nel costruttore: ", this.personaggi)
 
     monsterRepo.getAllMonsters().subscribe(resp=> this.mostri = resp);
-    Console.log("Mostri caricati nel costruttore: ", this.mostri)
   }
 }
