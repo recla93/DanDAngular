5602853c07b9ba84b961dfe1b9008f6be7a026d4
Vincenzo-git6 <vincenzomascolo6@gmail.com>
2025-05-12 18:45:32 +0200

    boh

--- src/components/gameArch/actionShow/action-log.component.ts
+++ src/components/gameArch/actionShow/action-log.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+// @ts-ignore
 @Component({
   selector: 'app-action-log',
   imports: [],
   templateUrl: './action-log.component.html',
+  standalone: true,
   styleUrl: './action-log.component.css'
 })
 export class ActionLogComponent {
